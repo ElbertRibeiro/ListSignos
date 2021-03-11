47bcf35961eb9a487a1810243e80850491e697af
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2021-03-11 13:17:22 -0300

    Update index.js

--- src/components/Signos/index.js
+++ src/components/Signos/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { View } from 'react-native';
 
 
 import List from '../List';
@@ -8,7 +8,7 @@ import style from './style';
 export default function Signos() {
   return (
     
-    <ScrollView style={style.list}>
+    <View style={style.list}>
       <List signo="Aquário" dataInicio="21/01" dataFim="19/02" />
       <List signo="Peixes" dataInicio="20/02" dataFim="20/03" />
       <List signo="Áries" dataInicio="21/03" dataFim="20/04" />
@@ -21,7 +21,7 @@ export default function Signos() {
       <List signo="Escorpião" dataInicio="24/10" dataFim="22/11" />
       <List signo="Sagitário" dataInicio="23/11" dataFim="21/12" />
       <List signo="Capricórnio" dataInicio="22/12" dataFim="20/01" />
-    </ScrollView>
+    </View>
     
   );
 }
